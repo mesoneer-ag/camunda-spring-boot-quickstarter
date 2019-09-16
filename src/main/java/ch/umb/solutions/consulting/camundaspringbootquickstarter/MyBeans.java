7934a01a1322811c7bb9697292924680b10e582f
Nikola Stankovic <nikola.stankovic@umb.ch>
2019-09-17 00:24:07 +0200

    implemented basic process app

---
+++ src/main/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/MyBeans.java
@@ -0,0 +1,15 @@
+package ch.umb.solutions.consulting.camundaspringbootquickstarter;
+
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBeans {
+
+  @Bean
+  public JavaDelegate ahoyService() {
+    return execution -> System.out.println("\n\nAhoy, " + execution.getVariable("myVariable") + "!");
+  }
+
+}
---
+++ src/main/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/ProcessApp.java
@@ -0,0 +1,13 @@
+package ch.umb.solutions.consulting.camundaspringbootquickstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
+
+@SpringBootApplication
+public class ProcessApp {
+
+  public static void main(String... args) {
+    SpringApplication.run(ProcessApp.class);
+  }
+
+}
