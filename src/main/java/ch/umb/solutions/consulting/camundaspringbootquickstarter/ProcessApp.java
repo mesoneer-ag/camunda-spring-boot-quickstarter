4fa2f02daa093a2f80d21c3b7c7c91340147e83c
Nikola Stankovic <nikola.stankovic@umb.ch>
2019-09-17 14:34:33 +0200

    add camunda archives properties

--- src/main/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/ProcessApp.java
+++ src/main/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/ProcessApp.java
@@ -1,9 +1,11 @@
 package ch.umb.solutions.consulting.camundaspringbootquickstarter;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class ProcessApp {
 
     public static void main(String... args) {
