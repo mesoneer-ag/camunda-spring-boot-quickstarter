7eef9a72393076e49aa10896427d8b4d2c5a6751
Dirk Budke <dirk.budke@umb.ch>
2020-09-25 23:29:44 +0200

    feat: add type-safe process variable framework

--- src/main/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/ProcessConstants.java
+++ src/main/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/ProcessConstants.java
@@ -3,7 +3,5 @@ package ch.umb.solutions.consulting.camundaspringbootquickstarter;
 public class ProcessConstants {
 
     public static final String PROCESS_DEFINITION_SAMPLE_PROCESS = "sample-process";
-    public static final String VARIABLE_LOGMESSAGE = "logMessage";
-    public static final String VARIABLE_AMOUNT = "amount";
 
 }
