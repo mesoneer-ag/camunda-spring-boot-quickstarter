909d83369006fb1aaeb8bd978dbaa0813b741cdf
Dirk Budke <dirk.budke@umb.ch>
2020-10-11 00:40:04 +0200

    fix: use unique process engine name in spring boot ProcessIntegrationTest

--- src/test/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/ProcessIntegrationTest.java
+++ src/test/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/ProcessIntegrationTest.java
@@ -1,70 +1,100 @@
 package ch.umb.solutions.consulting.camundaspringbootquickstarter;
 
-import ch.umb.solutions.consulting.camundaspringbootquickstarter.delegate.SampleDelegate;
 import ch.umb.solutions.consulting.camundaspringbootquickstarter.mock.LoggerDelegateMock;
+import ch.umb.solutions.consulting.camundaspringbootquickstarter.mock.MockHelper;
 import org.apache.ibatis.logging.LogFactory;
-import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions;
-import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static ch.umb.solutions.consulting.camundaspringbootquickstarter.mock.MockHelper.registerMock;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
-
 
 
 /**
  * Test case starting an in-memory database-backed Process Engine using SpringBoot
- * without test coverage report
+ * without test coverage report.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ProcessApp.class})
-public class ProcessIntegrationTest extends AbstractProcessEngineRuleTest {
+/**
+ * https://docs.camunda.org/manual/7.13/user-guide/spring-boot-integration/testing/
+ */
+@SpringBootTest(
+        properties = {
+                "camunda.bpm.generate-unique-process-engine-name=true",
+                "camunda.bpm.generate-unique-process-application-name=true",
+                "camunda.bpm.job-execution.enabled=false",
+                "spring.datasource.generate-unique-name=true"
+                // additional properties...
+        }
+)
+public class ProcessIntegrationTest  {
 
-  private static final String PROCESS_DEFINITION_KEY = "job-executor-test";
+    private static final String PROCESS_DEFINITION_KEY = "job-executor-test";
 
-  static {
-    LogFactory.useSlf4jLogging(); // MyBatis
-  }
-
-  @Autowired
-  RuntimeService runtimeService;
+    static {
+        LogFactory.useSlf4jLogging(); // MyBatis
+    }
 
 
-  @Autowired
-  ApplicationContext context;
+    @Autowired
+    ProcessEngine processEngine;
 
-  @Before
-  public void registerMocks() {
-    // Work around since the Camunda MockExpressionManager does not seem to work
-    registerMock(context, "loggerDelegate", LoggerDelegateMock.class);
-    registerMock(context, "sampleDelegate", SampleDelegate.class);
-  }
+    @Autowired
+    ApplicationContext context;
 
-  @Test
-  public void testHappyPath() {
+    @Before
+    public void registerMocks() {
+        MockHelper.replaceWithMock(context,"loggerDelegate", LoggerDelegateMock.class);
+        init(processEngine);
+    }
 
-    // Either: Drive the process by API and assert correct behavior by camunda-bpm-assert, e.g.:
-    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
-    execute(job(processInstance));
-    execute(job(processInstance));
-    execute(job(processInstance));
-    assertThat(processInstance).isEnded();
-    // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
-  }
+    @Test
+    public void testHappyPath() {
 
-  /** Clean up the process engine from ThreadLocal to allow other test cases to run properly */
-  @AfterClass
-  public static void cleanUp() {
-    AbstractAssertions.reset();
-  }
+       ProcessInstance processInstance = processEngine.getRuntimeService()
+                .startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+        execute(job(processInstance));
+        execute(job(processInstance));
+        execute(job(processInstance));
+
+        assertThat(processInstance).isEnded();
+    }
+
+    @Test
+    public void testExceptionPath() {
+
+        ProcessInstance processInstance = processEngine.getRuntimeService()
+                .startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+        execute(job(processInstance));
+        execute(job(processInstance));
+        execute(job(processInstance));
+
+        assertThat(processInstance).isEnded();
+    }
+
+    /**
+     * Clean up the process engine from ThreadLocal to allow other test cases to run properly
+     */
+    @AfterClass
+    public static void cleanUp() {
+        AbstractAssertions.reset();
+    }
+
+
 
 }
--- src/test/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/mock/MockHelper.java
+++ src/test/java/ch/umb/solutions/consulting/camundaspringbootquickstarter/mock/MockHelper.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.ApplicationContext;
 
+/**
+ *  Work around since the Camunda MockExpressionManager does not seem to work for Spring Boot configurations
+ *  Examples:
+ *  replaceWithMock(context, "loggerDelegate", LoggerDelegateMock.class);
+ *  replaceWithMock(context, "sampleDelegate", SampleDelegate.class);
+ */
 public class MockHelper {
-    public static void registerMock(ApplicationContext context, String beanName, Class className) {
+    public static void replaceWithMock(ApplicationContext context, String beanName, Class className) {
         GenericBeanDefinition mockBean = new GenericBeanDefinition();
         mockBean.setBeanClass(className);
-        ((BeanDefinitionRegistry) context).removeBeanDefinition(beanName);
-        ((BeanDefinitionRegistry) context).registerBeanDefinition(beanName, mockBean);
-
-
+        BeanDefinitionRegistry beanDefinitionRegistry = (BeanDefinitionRegistry) context;
+        if ( beanDefinitionRegistry.containsBeanDefinition(beanName)) {
+            beanDefinitionRegistry.removeBeanDefinition(beanName);
+        }
+        beanDefinitionRegistry.registerBeanDefinition(beanName, mockBean);
     }
 }
