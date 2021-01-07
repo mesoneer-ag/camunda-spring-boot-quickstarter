289adccab0916a53ae1c249c0a63de878d3387c4
Dirk Budke <dirk.budke@umb.ch>
2021-01-07 16:26:57 +0100

    feat: add default Camunda basic auth filter for Camunda REST API

--- src/main/java/ch/umb/solutions/consulting/camunda/springbootquickstarter/config/WebSecurityConfig.java
+++ src/main/java/ch/umb/solutions/consulting/camunda/springbootquickstarter/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package ch.umb.solutions.consulting.camunda.springbootquickstarter.config;
 
+import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,4 +25,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    /**
+     * Camunda REST API Basic Authentication configuration
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean processEngineAuthenticationFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setName("camunda-auth");
+        registration.setFilter(getProcessEngineAuthenticationFilter());
+        registration.addInitParameter("authentication-provider",
+                "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
+        registration.addUrlPatterns("/engine-rest/*");
+        return registration;
+    }
+
+    @Bean
+    public Filter getProcessEngineAuthenticationFilter() {
+        return new ProcessEngineAuthenticationFilter();
+    }
 }
