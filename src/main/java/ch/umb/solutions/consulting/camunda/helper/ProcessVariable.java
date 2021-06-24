51a26ed2e66443cecaaaffe8ab2f3758ffe88189
Fabian Eberle <fabian.eberle@umb.ch>
2021-06-24 13:10:06 +0200

    add feature to serialize objects as json

--- src/main/java/ch/umb/solutions/consulting/camunda/helper/ProcessVariable.java
+++ src/main/java/ch/umb/solutions/consulting/camunda/helper/ProcessVariable.java
@@ -1,13 +1,37 @@
 package ch.umb.solutions.consulting.camunda.helper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Set;
 import org.camunda.bpm.engine.delegate.VariableScope;
+import org.camunda.spin.plugin.variable.SpinValues;
 
 public class ProcessVariable<T> {
 
+
+    private Class<T> typeParameterClass;
+
+    private ObjectMapper jacksonObjectMapper = new ObjectMapper();
+
+    private static Set<Class<?>> SIMPLE_CLASSES = Set.of(Boolean.class, Character.class, Byte.class, Short.class, Integer.class, Long.class, Float.class, Double.class, Void.class, String.class);
+
     private String variableName;
+
+    private ProcessVariable(String variableName, Class<T> typeParameterClass) {
+        this.typeParameterClass = typeParameterClass;
+        this.variableName = variableName;
+    }
 
     private ProcessVariable(String variableName) {
         this.variableName = variableName;
+    }
+
+    public static ProcessVariable create(String variableName) {
+        return new ProcessVariable(variableName);
+    }
+
+    public static ProcessVariable createAsJson(String variableName, Class typeParameterClass) {
+        return new ProcessVariable(variableName, typeParameterClass);
     }
 
     public String getVariableName() {
@@ -15,16 +39,29 @@ public class ProcessVariable<T> {
     }
 
     public T get(VariableScope variableScope) {
-
-        return (T) variableScope.getVariable((variableName));
+        Object variable =  variableScope.getVariable(variableName);
+        if (typeParameterClass == null) {
+            return (T) variable;
+        } else {
+            try {
+                return jacksonObjectMapper.readValue(variable.toString(), typeParameterClass);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 
-    public void set(VariableScope variableScope, T variableValue) {
-        variableScope.setVariable(variableName, variableValue);
+    public void set(VariableScope variableScope, T variableValue) throws JsonProcessingException {
+        if (typeParameterClass == null) {
+            variableScope.setVariable(variableName, variableValue);
+        } else {
+            var serializedValue = jacksonObjectMapper.writeValueAsString(variableValue);
+            var jsonValue = SpinValues.jsonValue(serializedValue);
+            variableScope.setVariable(variableName, jsonValue);
+        }
     }
 
-    public static ProcessVariable create(String variableName) {
-        return new ProcessVariable(variableName);
-    }
+
 
 }
--- src/main/java/ch/umb/solutions/consulting/camunda/springbootquickstarter/delegate/SampleDelegate.java
+++ src/main/java/ch/umb/solutions/consulting/camunda/springbootquickstarter/delegate/SampleDelegate.java
@@ -3,12 +3,12 @@ package ch.umb.solutions.consulting.camunda.springbootquickstarter.delegate;
 import ch.umb.solutions.consulting.camunda.springbootquickstarter.service.IErpService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
 import javax.inject.Inject;
 
+import static ch.umb.solutions.consulting.camunda.springbootquickstarter.ProcessVariables.VAR_FOO;
 import static ch.umb.solutions.consulting.camunda.springbootquickstarter.ProcessVariables.VAR_SAMPLE;
 
 
@@ -31,6 +31,10 @@ public class SampleDelegate implements JavaDelegate {
     public void execute(DelegateExecution execution) throws Exception {
         logger.debug("Get variable: " + VAR_SAMPLE.getVariableName());
         var s = VAR_SAMPLE.get(execution);
+
+        var foo = VAR_FOO.get(execution);
+
+        logger.debug("foo: "+ foo.toString());
 
         erpService.updateErp(s);
         logger.debug("Set variable: " + VAR_SAMPLE.getVariableName());
